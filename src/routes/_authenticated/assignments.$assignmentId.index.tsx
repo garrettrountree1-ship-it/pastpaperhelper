f473f34699675deaf58b5506718535380b4381a3
garrettrountree1-ship-it <garrett.rountree1@gmail.com>
2026-09-17 16:55:39 +0800

    Avoid Paper Mode add-add conflicts

--- src/routes/_authenticated/assignments.$assignmentId.index.tsx
+++ src/routes/_authenticated/assignments.$assignmentId.index.tsx
@@ -18,7 +18,7 @@ import { StudentTutorControls } from "@/components/assignments/StudentTutorContr
 import { useActiveTime } from "@/hooks/use-active-time";
 import { useContentProtection } from "@/hooks/use-content-protection";
 import { QuestionExperience } from "@/components/assignments/QuestionExperience";
-import { StudentPaperMode } from "@/components/assignments/PaperMode";
+import { StudentPaperMode } from "@/components/assignments/ContinuousPaperMode";
 import { PAD_FILE_NAME } from "@/components/assignments/DrawingPad";
 import { parseSnipBand } from "@/components/assignments/QuestionSnip";
 import {
